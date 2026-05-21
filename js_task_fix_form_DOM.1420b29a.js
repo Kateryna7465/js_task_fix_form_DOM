b30d7b0061cf0466508be058831e2738e3b4ede7
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-05-21 15:57:40 +0200

    make build

---
+++ js_task_fix_form_DOM.1420b29a.js
@@ -0,0 +1,2 @@
+"use strict";for(var inputs=document.querySelectorAll("form input"),i=0;i<inputs.length;i++){var e=inputs[i],t=document.createElement("label");t.className="field-label",t.setAttribute=e.id;var r=e.name.charAt(0).toUpperCase()+e.name.slice(1);t.textContent=r,e.placeholder=r,e.parentElement.insertBefore(t,e)}
+//# sourceMappingURL=js_task_fix_form_DOM.1420b29a.js.map
