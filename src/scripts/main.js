6d2e6dbd57dadb31b15005ab98599d385df0d61b
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-05-21 15:49:32 +0200

    solved mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,16 @@
 'use strict';
 
-const inputs = document.querySelectorAll("form input");
+const inputs = document.querySelectorAll('form input');
 
 for (let i = 0; i < inputs.length; i++) {
   const input = inputs[i];
 
-  const label = document.createElement("label");
-  label.classname = "field-labe";
-  label.setAttribute = ("for", input.id);
+  const label = document.createElement('label');
 
-  const text =
-  input.name.charAt(0).toUpperCase() +
-  input.name.slice(1);
+  label.className = 'field-label';
+  label.setAttribute = ('for', input.id);
+
+  const text = input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
   label.textContent = text;
 
